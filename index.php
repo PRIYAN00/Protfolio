4f58e5b6d246925ee18223c4ce262746917bcfee
PRIYAN00 <mahindarajpriyanthan@outlook.com>
2023-10-24 08:34:08 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php include('header.php'); ?>
+<body class="bg-dark d-flex flex-column min-vh-100">
+<div class="container py-4">
+
+
+    <!--- Header Name Banner -->            
+    <div class="text-white bg-dark text-center py-5">
+        <p class="Teko_font display-4">Hey 👋 I'm</p>
+        <img src="img/Cover_Kocha.png" style="width: 300px;" class="rounded">
+        <p class="Lobster_font display-1">Priyanthan Mahindaraj</p>
+        <span class="Teko_font display-4 text-uppercase"> DEVELOPER - DESIGNER - IT Support Specialist </span><br>
+        <div class="btn-group my-2">
+            <a class="btn btn-outline-light" href="https://github.com/PRIYAN00" target="_blank"><i class="fa fa-github"></i> GitHub </a>
+            <a class="btn btn-outline-primary"  href="https://www.linkedin.com/in/priyanthan00/" target="_blank"> <i class="fa fa-linkedin"></i> Linkdin</a>
+        </div>
+    </div>
+    <!--- Header Name Banner -->
+    
+</div>
+</body>
+<?php include('footer.php'); ?>
